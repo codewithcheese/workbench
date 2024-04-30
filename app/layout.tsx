a363042308938ced7007fca0f92be39c8ffb38e4
codewithcheese <mr.tmanning@gmail.com>
2024-04-30 23:16:30 +1000

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Baseline",
+  title: "Workbench",
   description: "Iteratively create and test magical system prompts",
 };
 
