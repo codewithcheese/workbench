167c0f8786efefbaea793cab6827d87b58c99521
codewithcheese <mr.tmanning@gmail.com>
2024-05-09 16:51:31 +1000

    Redirect index to first project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+    import {goto} from '$app/navigation';
+    import {db} from '@/store.svelte';
+
+    if (db.projects.items.length) {
+      const project = db.projects.items[0];
+      goto(`/project/${project.id}`);
+    }
+
+</script>
