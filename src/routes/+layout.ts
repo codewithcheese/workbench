2a62bb5bdf19b7a2caac57b9230f28d20e484e9d
codewithcheese <mr.tmanning@gmail.com>
2024-05-11 22:02:09 +1000

    Try migration in root layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,13 @@
 import "../app.css";
 import "@fontsource-variable/inter";
+import { browser } from "$app/environment";
 
 export const ssr = false;
+
+if (browser) {
+  // migrate database
+  console.log("Migrating database");
+  const { migrator } = await import("@/database/migrator");
+  const results = await migrator.migrateToLatest();
+  console.log("Migration results", results);
+}
