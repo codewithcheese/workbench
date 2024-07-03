606f32cca72ad95d49fd2436ad3880ce25d73e96
codewithcheese <mr.tmanning@gmail.com>
2024-07-04 01:56:06 +1000

    Enable foreign key checks

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -1,6 +1,7 @@
 import "../../app.css";
 import "@fontsource-variable/inter";
 import { exposeDb, projectTable, registerModel, useDb } from "@/database";
+import { sql } from "drizzle-orm/sql";
 
 export const ssr = false;
 let migrated = false;
@@ -13,6 +14,7 @@ export async function load({ depends }) {
       const { runMigrations } = await import("@/database/migrator");
       console.log("Migrating database");
       await runMigrations();
+      await useDb().run(sql.raw("PRAGMA foreign_keys=on;"));
       console.log("Migration complete");
       migrated = true;
     }
