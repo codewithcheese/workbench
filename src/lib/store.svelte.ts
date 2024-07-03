8e25548551707b7b51cc868fd6a14688262fc561
codewithcheese <mr.tmanning@gmail.com>
2024-07-03 19:29:34 +1000

    Remove dataset types and loaders

--- src/lib/store.svelte.ts
+++ src/lib/store.svelte.ts
@@ -1,79 +1,8 @@
-import { Dataset } from "@/lib/dataset.svelte";
-import { nanoid } from "nanoid";
-
-// todo: remove these types and datasets once migration deployed
-export type Document = {
-  id: string;
-  name: string;
-  description: string;
-  content: string;
-  data?: any;
-};
-
-export type Response = {
-  id: string;
-  projectId: string;
-  modelId: string;
-  error: string | null;
-};
-
-export type ResponseMessage = {
-  id: string;
-  responseId: string;
-  role: string;
-  content: string;
-};
-
-export type Model = {
-  id: string;
-  serviceId: string;
-  name: string;
-  visible: boolean;
-};
-
-export type Service = {
-  id: string;
-  name: string;
-  providerId: string;
-  baseURL: string;
-  apiKey: string;
-};
-
 export type Store = {
   selected: {
     modelId: string | null;
   };
 };
-
-export type Project = {
-  id: string;
-  name: string;
-  prompt: string;
-};
-
-export type DB = {
-  documents: Dataset<Document>;
-  projects: Dataset<Project>;
-  responses: Dataset<Response>;
-  services: Dataset<Service>;
-  models: Dataset<Model>;
-  messages: Dataset<ResponseMessage>;
-};
-
-export let db: DB = $state({
-  projects: Dataset.load<Project>("projects", [
-    {
-      id: nanoid(10),
-      name: "Untitled",
-      prompt: "",
-    },
-  ]),
-  documents: Dataset.load<Document>("documents"),
-  responses: Dataset.load<Response>("responses"),
-  services: Dataset.load<Service>("services"),
-  models: Dataset.load<Model>("models"),
-  messages: Dataset.load<ResponseMessage>("messages"),
-});
 
 const storeJson = localStorage.getItem("store");
 
