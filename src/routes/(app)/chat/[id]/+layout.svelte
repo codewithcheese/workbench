7ab86c02632554b6f838275dd280a315c90a657b
codewithcheese <mr.tmanning@gmail.com>
2024-07-14 21:01:44 +1000

    Adjust padding

--- src/routes/(app)/chat/[id]/+layout.svelte
+++ src/routes/(app)/chat/[id]/+layout.svelte
@@ -31,7 +31,7 @@
 </script>
 
 <header
-  class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background p-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-4 sm:pt-2"
+  class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 pt-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent"
 >
   <Tabs onValueChange={tabClick} value={data.tab}>
     <TabsList>
